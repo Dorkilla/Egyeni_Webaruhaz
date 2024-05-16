d0893671c70a7f50b01e5a5b8fa1023c9cc3fb4a
Dorkilla <dorkilla58@gmail.com>
2024-05-17 00:12:06 +0200

    kereses mukodik, rendezes egy nagy nulla

--- adatkezelo.js
+++ adatkezelo.js
@@ -1,13 +1,16 @@
-export function rendez(lista, irany) {
+export function rendez(lista, irany, szempont) {
   lista.sort(function (e1, e2) {
-    let eredmeny = 1;
-    if (e1.nev < e2.nev) {
-      eredmeny = -1;
+    let eredmeny = 0;
+    if (szempont === "nev") {
+      if (e1.nev < e2.nev) eredmeny = -1;
+      else if (e1.nev > e2.nev) eredmeny = 1;
+    } else if (szempont === "ar") {
+      eredmeny = e1.ar - e2.ar;
+    } else if (szempont === "meret") {
+      eredmeny = e1.meret - e2.meret;
     }
     return eredmeny * irany;
   });
-  
-  return lista;
 }
 
 export function szures(lista, keresoSzoveg) {
@@ -17,49 +20,49 @@ export function szures(lista, keresoSzoveg) {
   return szurtLISTA;
 }
 
-  var egyebOrszagSelect = document.getElementById("orszag");
-  var egyebOrszagDiv = document.getElementById("egyebOrszagDiv"); 
+var egyebOrszagSelect = document.getElementById("orszag");
+var egyebOrszagDiv = document.getElementById("egyebOrszagDiv");
 
-  egyebOrszagSelect.addEventListener("change", function() {
-    if (egyebOrszagSelect.value === "egyeb") {
-      egyebOrszagDiv.style.display = "block";
+egyebOrszagSelect.addEventListener("change", function () {
+  if (egyebOrszagSelect.value === "egyeb") {
+    egyebOrszagDiv.style.display = "block";
+  } else {
+    egyebOrszagDiv.style.display = "none";
+    document.getElementById("egyebOrszag").value = "";
+  }
+});
+
+$(document).ready(function () {
+  $("#varos").change(function () {
+    if ($(this).val() === "egyeb") {
+      $("#egyebVarosDiv").show();
     } else {
-      egyebOrszagDiv.style.display = "none";
-      document.getElementById("egyebOrszag").value = "";
+      $("#egyebVarosDiv").hide();
+      $("#egyebVaros").val("");
     }
   });
+});
 
-  $(document).ready(function() {
-    $("#varos").change(function() {
-      if ($(this).val() === "egyeb") {
-        $("#egyebVarosDiv").show();
-      } else {
-        $("#egyebVarosDiv").hide();
-        $("#egyebVaros").val("");
-      }
-    });
-  });
-  
 
-  $(document).ready(function() {
-    var idopontDiv = $("#idopontDiv");
-    var szemelyesAtvetelRadio = $("#szemelyes_atvetel");
-  
-    idopontDiv.hide(); // Kezdetben rejtve van az átvételi időpont mező
-  
-    szemelyesAtvetelRadio.change(function() {
-      if (this.checked)  {
-        idopontDiv.show();
-      } else {
-        idopontDiv.hide();
-      }
-    });
+$(document).ready(function () {
+  var idopontDiv = $("#idopontDiv");
+  var szemelyesAtvetelRadio = $("#szemelyes_atvetel");
+
+  idopontDiv.hide(); //Kezdetben rejtve 
+
+  szemelyesAtvetelRadio.change(function () {
+    if (this.checked) {
+      idopontDiv.show();
+    } else {
+      idopontDiv.hide();
+    }
   });
-  function uzenetEsemeny(uzenet) {
-    const rendelELEM = $(".rendel");
-    rendelELEM.on("click", function () {
-      window.alert(uzenet);
-    });
+});
+function uzenetEsemeny(uzenet) {
+  const rendelELEM = $(".rendel");
+  rendelELEM.on("click", function () {
+    window.alert(uzenet);
+  });
 }
 
 uzenetEsemeny("Rendelésed sikeresen leadva! Hamarosan felvesszük veled a kapcsolatot.");
--- main.js
+++ main.js
@@ -1,60 +1,90 @@
 import { KUBULISTA } from "./adatok.js";
-import { htmlOsszeallitKartyak, kartyakMegjelenitese, frissitKosar } from "./fuggvenyek.js";
+import { htmlOsszeallitKartyak, kartyakMegjelenitese } from "./fuggvenyek.js";
 import { rendez, szures } from "./adatkezelo.js";
 
-//Kártyák elrendezése
-function kartyakElrendezese() {
-  const kartyaContainer = $(".termekek");
-  const kartyaSzelesseg = kartyaContainer.find(".card").first().outerWidth(true); // Kártya szélessége
-  const containerSzelesseg = kartyaContainer.width(); // Kártya konténer szélessége
-  const kartyaSoronkent = Math.floor(containerSzelesseg / kartyaSzelesseg); // Kártyák száma egy sorban
-
-  //Kártyák konténerének szélessége soronként
-  kartyaContainer.css("max-width", kartyaSoronkent * kartyaSzelesseg + "px");
-}
-
-//Menüpontok közötti váltás
 $(document).ready(function () {
-  // Kezdetben csak a termékek és a kosár 
+  //Kártyák elrendezése
+  const kartyaContainer = $(".termekek");
+  const kartyaSzelesseg = kartyaContainer.find(".card").first().outerWidth(true); //Kártya szélessége
+  const containerSzelesseg = kartyaContainer.width(); //Kártya konténer szélessége
+  const kartyaSoronkent = Math.floor(containerSzelesseg / kartyaSzelesseg); //Kártyák száma egy sorban
+
+  //Kártyák k. szélessége soronként
+  kartyaContainer.css("max-width", kartyaSoronkent * kartyaSzelesseg + "px");
+
+  //Kezdetben csak a termékek és a kosár 
   $(".szemelyes-adatlap").hide();
 
-  // Termékek oldal
+  // Menüpontok közötti váltás
   $("#termekLink").click(function () {
     $(".termekek, .kosar-es-osszeg").show();
     $(".szemelyes-adatlap").hide();
   });
 
-  // Személyes adatlap
   $("#szemelyesAdatlapLink").click(function () {
     $(".termekek, .kosar-es-osszeg").hide();
     $(".szemelyes-adatlap").show();
   });
 });
 
-
 let irany = 1;
 let rendezettLista = [];
 let szLISTA = [];
 
-init(KUBULISTA);
-
 function init(lista) {
-  rendezettLista = rendez(lista, irany);
-  kartyakMegjelenitese(htmlOsszeallitKartyak(rendezettLista));
+  rendezettLista = rendez(lista, irany, "nev"); 
+  kartyakMegjelenitese(htmlOsszeallitKartyak(KUBULISTA));
   rendezEsemeny();
   szuresEsemeny();
 }
 
 function rendezEsemeny() {
-  const nevELEM = $("#NovekvoNev");
-  nevELEM.on("click", function () {
-    irany *= -1;
-    rendezettLista = rendez(KUBULISTA, irany);
+  const nevNovekvoELEM = $("#NovekvoNev");
+  const nevCsokkenoELEM = $("#CsokkenoNev");
+  const arNovekvoELEM = $("#NovekvoAr");
+  const arCsokkenoELEM = $("#CsokkenoAr");
+  const meretNovekvoELEM = $("#NovekvoMeret");
+  const meretCsokkenoELEM = $("#CsokkenoMeret");
+
+  nevNovekvoELEM.on("click", function () {
+    irany = 1;
+    rendezettLista = rendez(KUBULISTA, irany, "nev");
+    kartyakMegjelenitese(htmlOsszeallitKartyak(rendezettLista));
+  });
+
+  nevCsokkenoELEM.on("click", function () {
+    irany = -1;
+    rendezettLista = rendez(KUBULISTA, irany, "nev");
+    kartyakMegjelenitese(htmlOsszeallitKartyak(rendezettLista));
+  });
+
+  arNovekvoELEM.on("click", function () {
+    irany = 1;
+    rendezettLista = rendez(KUBULISTA, irany, "ar");
+    kartyakMegjelenitese(htmlOsszeallitKartyak(rendezettLista));
+  });
+
+  arCsokkenoELEM.on("click", function () {
+    irany = -1;
+    rendezettLista = rendez(KUBULISTA, irany, "ar");
+    kartyakMegjelenitese(htmlOsszeallitKartyak(rendezettLista));
+  });
+
+  meretNovekvoELEM.on("click", function () {
+    irany = 1;
+    rendezettLista = rendez(KUBULISTA, irany, "meret");
+    kartyakMegjelenitese(htmlOsszeallitKartyak(rendezettLista));
+  });
+
+  meretCsokkenoELEM.on("click", function () {
+    irany = -1;
+    rendezettLista = rendez(KUBULISTA, irany, "meret");
     kartyakMegjelenitese(htmlOsszeallitKartyak(rendezettLista));
   });
 }
 
 function szuresEsemeny() {
+  //Keresés esemény 
   const keresoELEM = $("#szuro");
   keresoELEM.on("keyup", function () {
     let keresoSzoveg = keresoELEM.val();
@@ -62,6 +92,23 @@ function szuresEsemeny() {
     kartyakMegjelenitese(htmlOsszeallitKartyak(szLISTA));
   });
 }
+
+//Rendelés leadás
+$(document).ready(function () {
+  $(".rendel").on("click", function () {
+    window.alert("Rendelésed sikeresen leadva! Hamarosan felvesszük veled a kapcsolatot.");
+  });
+});
+
+init(KUBULISTA);
+
+
+
+
+
+
+
+
 
 
 
